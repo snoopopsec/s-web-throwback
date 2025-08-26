adbfe2c76ab622cf66034235461e5e9209c98a0d
snoopopsec <92749864+snoopopsec@users.noreply.github.com>
2025-08-26 13:18:21 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/s-web-throwback">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
